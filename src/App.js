1f458c9a7b1d73495d81122e3299356b9bace709
Chinmay Patil <chinmaypatil@192.168.1.3>
2022-11-16 08:43:19 +0530

    uploading FE

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+// import FirstMenu from "./components/firstmenu/FirstMenu";
+import { useState } from 'react';
+import contactlist from './components/contactlist'
+import {
+  Button, Heading, Text, Input, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon,
+  Box, Table, Thead, Tbody, Tr, Th, Td, TableContainer,
+} from '@chakra-ui/react'
+
+
+import './App.css'
+import axios from 'axios';
 
 function App() {
+  const [MsgToSend, setMsgToSend] = useState("");
+  const [listOfMsgSent, setlistOfMsgSent] = useState([]);
+
+
+  function userToSendMsg(userdata) {
+
+    if (MsgToSend) {
+
+      userdata.message = MsgToSend;
+      userdata.timestamp = new Date().toUTCString()
+      // console.log(userdata);
+
+      axios.post("http://localhost:3000/send", {
+        userdata
+      })
+        .then(function (response) {
+          // console.log(response.data)
+          setlistOfMsgSent([response.data, ...listOfMsgSent])
+
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+
+    else alert("enter message")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='contactLists'>      <Heading size='sm'>Contact List</Heading>
+        {contactlist.map(person => {
+          return (
+            <div key={person.id}>
+              <Accordion allowMultiple>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton _expanded={{ bg: 'tomato', color: 'white' }}>
+
+                      <Box flex='1' textAlign='left'>
+                        {person.name}
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4}>
+                    <Text>  <b>Name :</b>  {person.name}</Text>
+                    <Text> <b>Email :</b> {person.email} </Text>
+                    <Text><b>Contact Number : </b>{person.phone}</Text>
+                    <Input placeholder='Enter message to send' onChange={((e) => setMsgToSend(e.target.value))} />
+                    <Button onClick={() => userToSendMsg(person)}>Send Message</Button>
+                  </AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+            </div>
+          )
+        })}
+      </div>
+
+      <div>
+        <div>
+          <Text fontSize='2xl'>List of Messages sent</Text>
+          <TableContainer>
+            <Table variant='striped'>
+
+              <Thead>
+                <Tr>
+                  <Th>Name</Th>
+                  <Th>Email</Th>
+                  <Th >Contact Number</Th>
+                  <Th >Message & OTP</Th>
+                  <Th >Timestamp</Th>
+                </Tr>
+              </Thead>
+            </Table>
+          </TableContainer>
+        </div>
+
+        {listOfMsgSent.map((ele) => {
+          return (
+            <div>
+              <TableContainer>
+                <Table variant='striped'>
+
+                  <Tbody>
+                    <Tr>
+                      <Td>{ele.name}</Td>
+                      <Td> {ele.email}</Td>
+                      <Td>{ele.phone}</Td>
+                      <Td> {ele.message}</Td>
+                      <Td >{ele.timestamp}</Td>
+                    </Tr>
+
+                  </Tbody>
+
+                </Table>
+              </TableContainer>
+
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/contactlist.js
@@ -0,0 +1,35 @@
+
+const contactLists = [
+    {
+      "id": 1,
+      "name": "Test User",
+      "email": "test@udsf.com",
+      "phone": "+918390660066"
+    },
+    {
+      "id": 2,
+      "name": "Test User 2",
+      "email": "knappberry@unq.com",
+      "phone": "+919810153260"
+    },
+    {
+      "id": 3,
+      "name": "Bell Burgess",
+      "email": "bellburgess@unq.com",
+      "phone": "+1 (887) 478-2693"
+    },
+    {
+      "id": 4,
+      "name": "Hobbs Ferrell",
+      "email": "hobbsferrell@unq.com",
+      "phone": "+1 (862) 581-3022"
+    },
+    {
+      "id": 5,
+      "name": "Marylou Medina",
+      "email": "maryloumedina@unq.com",
+      "phone": "+1 (996) 520-2967"
+    }
+  ];
+
+  export default contactLists
